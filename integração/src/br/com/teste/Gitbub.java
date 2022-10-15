df6eb28bb24956c849b124afd0c411580f31db18
Pedro Soares <pedroneto.ps180@gmail.com>
2022-10-15 10:07:31 -0300

    teste

---
+++ integração/src/br/com/teste/Gitbub.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.com.teste;
+
+/**
+ * @author pedro.soares
+ *
+ */
+public class Gitbub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("teste");
+
+	}
+
+}
